fbd160f88246ff67e561d57a9b69b67e50488c09
최미현 <mimichoi@ajou.ac.kr>
2025-12-07 05:49:08 +0900

    .....

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 // backend/routes/auth.js
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const pool = require('../db');
 
